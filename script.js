7e98a8e45b275f54e6f4d9d39857fc4fa3f4640d
Tech Nik <tech.nik.effects@gmail.com>
2023-11-14 19:47:15 +0100

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+function btn_1() {
+  navigator.clipboard.writeText("➜");
+}
+
+
+
+
+
+
+
+
+window.addEventListener("keydown", key_event, false)
+
+function key_event(key) {
+       
+    if (key.keyCode == "49") {
+        btn_1()
+        }
+            
+
+
+
+
+    }
