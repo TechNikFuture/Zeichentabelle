f791866c41cb036519e890dc035e4166832434ea
Tech Nik <tech.nik.effects@gmail.com>
2023-11-19 16:39:14 +0100

    s

--- script.js
+++ script.js
@@ -7,21 +7,20 @@ function btn_2() {
 function btn_3() {
   navigator.clipboard.writeText("³");
 }
-
-
-function btn_shift_2() {
-  navigator.clipboard.writeText("₂");
-}
-function btn_shift_3() {
-  navigator.clipboard.writeText("₃");
-}
-
-
 function btn_4() {
   navigator.clipboard.writeText("< ");
 }
 function btn_5() {
   navigator.clipboard.writeText("> ");
+}
+
+
+
+function btn_W() {
+  navigator.clipboard.writeText("₂");
+}
+function btn_E() {
+  navigator.clipboard.writeText("₃");
 }
 
 
@@ -36,24 +35,16 @@ function key_event(key) {
 
     
     if (key.keyCode == "49") {
-        btn_1()
+      btn_1()
       }
       
-      if (key.keyCode == "50") {
-        if (shift_aktiv === true) {
-          btn_shift_2()
-        } else {
-        btn_2()
-        }
-        }
+    if (key.keyCode == "50") {
+      btn_2()
+      }
         
-        if (key.keyCode == "51") {
-          if (shift_aktiv === true) {
-            btn_shift_3()
-          } else {
-          btn_3()
-          }
-        }
+    if (key.keyCode == "51") {
+      btn_3()
+      }
         
     if (key.keyCode == "52") {
       btn_4()
@@ -62,15 +53,14 @@ function key_event(key) {
     if (key.keyCode == "53") {
       btn_5()
     }
+            
+    if (key.keyCode == "87") {
+      btn_W()
+    }
+            
+    if (key.keyCode == "69") {
+      btn_E()
+    }
     
-
-  if (key.keyCode == "16") {
-    shift_aktiv = true
-    setTimeout(function(){
-      shift_aktiv = false
-   }, 600);
-  }
-
-
 
     }
